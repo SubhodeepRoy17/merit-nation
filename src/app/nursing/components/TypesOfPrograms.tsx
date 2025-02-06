dfd9f934e044ca50982f512cd4a9faab2a63780f
SoumyadipRoy16 <roysoumyadip334@gmail.com>
2025-02-06 21:21:10 +0530

    More minor changes in the update-3

--- src/app/nursing/components/TypesOfPrograms.tsx
+++ src/app/nursing/components/TypesOfPrograms.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 interface TypesOfProgramsProps {
   activeProgram: "B.Sc" | "M.Sc"
